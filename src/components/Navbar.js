c13b188de202ac3289f8d47b125376cdc1e3cb1f
Moksh Kukreja <98658751+MokshKukreja@users.noreply.github.com>
2023-05-17 00:02:06 +0530

    last

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar(props) {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className={`navbar-brand  text-${props.mode === `light` ? `dark` : `light`}`} href="/">Home</a>
+          <a className={`navbar-brand  text-${props.mode === `light` ? `dark` : `light`}`} href="/#">Home</a>
         </li>
         {/* <li className="nav-item">
           <a className={`navbar-brand  text-${props.mode === `light` ? `dark` : `light`}`} href="/about">About</a>
