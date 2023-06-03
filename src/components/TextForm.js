ef6f226fa2a8279d67da2c887fc95153fe352735
Moksh Kukreja <98658751+MokshKukreja@users.noreply.github.com>
2023-06-03 17:26:24 +0530

    6th

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -17,16 +17,16 @@ function TextForm(props) {
 
     const Uppercase = ()=>{
         setText(text.toUpperCase())
-        props.alert("Converted to Uppercase","success")
+        // props.alert("Converted to Uppercase","success")
     }
     const LowerCase = ()=>{
         setText(text.toLowerCase())
-        props.alert("Converted to Lowercase","success")
+        // props.alert("Converted to Lowercase","success")
     }
     const ClearText = ()=>{
         setText("")
         setWordCount(0)
-        props.alert("Text is cleared","success")
+        // props.alert("Text is cleared","success")
     }
     return (
         <>
@@ -34,9 +34,11 @@ function TextForm(props) {
             <div className="mt-5">
                 <textarea className="form-control" onChange={handleChange} value={text} placeholder="Enter Text here" rows="10" ></textarea>
             </div>
-            <button type="button" onClick={Uppercase} className={`btn  ml-0`} style={{backgroundColor:props.color,color:props.color==="#303030"?"white":"#303030"}}>Convert to Uppercase</button>
+          <div className=''>
+          <button type="button" onClick={Uppercase} className={`btn  m-2 `} style={{backgroundColor:props.color,color:props.color==="#303030"?"white":"#303030"}}>Convert to Uppercase</button>
             <button type="button" onClick={LowerCase} className="btn m-2 " style={{backgroundColor:props.color,color:props.color==="#303030"?"white":"#303030"}}>Convert to LowerCase</button>
-            <button type="button" onClick={ClearText} className="btn m-0.75 " style={{backgroundColor:props.color,color:props.color==="#303030"?"white":"#303030"}}>Clear Text</button>
+            <button type="button" onClick={ClearText} className="btn m-2 " style={{backgroundColor:props.color,color:props.color==="#303030"?"white":"#303030"}}>Clear Text</button>
+          </div>
         </div>
         <div className='container my-3'>
             <p style={{color:props.color}}>{wordCount} words and {text.length} characters</p>
