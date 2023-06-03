87cc535f2d3a02d2a57049c86001770d3dc348ac
Moksh Kukreja <98658751+MokshKukreja@users.noreply.github.com>
2023-06-03 20:29:37 +0530

    7

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -28,6 +28,12 @@ function TextForm(props) {
         setWordCount(0)
         // props.alert("Text is cleared","success")
     }
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(text);
+    // props.alert("Text copied to clipboard", "success");
+  };
+  
     return (
         <>
         <div className='container'>
@@ -38,6 +44,7 @@ function TextForm(props) {
           <button type="button" onClick={Uppercase} className={`btn  m-2 `} style={{backgroundColor:props.color,color:props.color==="#303030"?"white":"#303030"}}>Convert to Uppercase</button>
             <button type="button" onClick={LowerCase} className="btn m-2 " style={{backgroundColor:props.color,color:props.color==="#303030"?"white":"#303030"}}>Convert to LowerCase</button>
             <button type="button" onClick={ClearText} className="btn m-2 " style={{backgroundColor:props.color,color:props.color==="#303030"?"white":"#303030"}}>Clear Text</button>
+            <button type="button" onClick={handleCopy} className="btn m-2 " style={{backgroundColor:props.color,color:props.color==="#303030"?"white":"#303030"}}>Copy Text Text</button>
           </div>
         </div>
         <div className='container my-3'>
